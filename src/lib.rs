7a49fdf20a132741e8b41c7c4dba39709dc5c5bb
Mystixs3 <mystixs3@noreply.localhost>
2022-12-23 16:58:08 +0000

    Woah, opcodes 0x00 to 0x0E done

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,7 @@ pub fn assemble(input: Mnemonic) -> Result<Instruction, String> {
 
 	// BRK [impl]
 	if input.inst == "BRK" {}
+
 	// ORA
 	else if input.inst == "ORA" {
 		// ORA (ind,X)
@@ -33,12 +34,61 @@ pub fn assemble(input: Mnemonic) -> Result<Instruction, String> {
 			output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "(${},X)", [hex u8]).unwrap()));
 		}
 		// ORA zpg
-		if input.args.clone().unwrap_or(String::from("")).starts_with("$") {
+		else if input.args.clone().unwrap_or(String::from("")).starts_with("$") && input.args.clone().unwrap_or(String::from("")).len() <= 3 {
 			output.opcode = 0x05;
 			output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u8]).unwrap()));
 		}
+		// ORA imm
+		else if input.args.clone().unwrap_or(String::from("")).starts_with("#") {
+			if input.args.clone().unwrap_or(String::from("")).starts_with("#$") {
+				output.opcode = 0x09;
+				output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "#${}", [hex u8]).unwrap()));
+			}
+			else {
+				output.opcode = 0x09;
+				output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "#{}", u8).unwrap()));
+			}
+		}
+		// ORA abs
+		else if input.args.clone().unwrap_or(String::from("")).starts_with("$")  && input.args.clone().unwrap_or(String::from("")).len() >= 3 {
+			output.opcode = 0x0D;
+			output.operand = Some(Operand::Full(scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u16]).unwrap()));
+		}
+		else {
+			return Err(String::from("The inputted mnemonic didn't match with anything"));
+		}
 	}
 
+	// ASL
+	else if input.inst == "ASL" {
+		// ASL zpg
+		if input.args.clone().unwrap_or(String::from("")).starts_with("$") && input.args.clone().unwrap_or(String::from("")).len() <= 3 {
+			output.opcode = 0x06;
+			output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u8]).unwrap()));
+		}
+		// ASL [accum]
+		else if input.args.clone().unwrap_or(String::from("")) == "" || input.args.clone().unwrap_or(String::from("")) == "A" {
+			output.opcode = 0x0A;
+			output.operand = None;
+		}
+		// ASL abs
+		else if input.args.clone().unwrap_or(String::from("")).starts_with("$")  && input.args.clone().unwrap_or(String::from("")).len() >= 3 {
+			output.opcode = 0x0E;
+			output.operand = Some(Operand::Full(scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u16]).unwrap()));
+		} 
+		else {
+			return Err(String::from("The inputted mnemonic didn't match with anything"));
+		}
+	}
+
+	// PHP [impl]
+	else if input.inst == "PHP" {
+		output.opcode = 0x08;
+		output.operand = None;
+	}
+
+
+	// *Incredibly* basic errors
 	else {
 		return Err(String::from("The inputted mnemonic didn't match with anything"));
 	}
