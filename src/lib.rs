75921346a5ed9e17b1faf658b05eebb14c8d0cc0
Mystixs3 <mystixs3@noreply.localhost>
2023-02-25 12:36:58 +0000

    File I/O, relative addressing, stuff like that

--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,174 @@
-#[macro_use] extern crate scan_fmt;
+#[macro_use]
+extern crate scan_fmt;
 
 #[derive(Debug)]
 pub struct Mnemonic {
-	pub inst: String,
-	pub args: Option<String>,
+    pub inst: String,
+    pub args: Option<String>,
 }
 
 #[derive(Debug)]
 pub struct Instruction {
-	pub opcode: u8,
-	pub operand: Option<Operand>,
+    pub opcode: u8,
+    pub operand: Option<Operand>,
 }
 
 #[derive(Debug)]
 pub enum Operand {
-	Half(u8),
-	Full(u16),
+    Half(u8),
+    Full(u16),
 }
 
-pub fn assemble(input: Mnemonic) -> Result<Instruction, String> {
-	let mut output = Instruction { opcode: 0x00, operand: None };
+pub fn assemble(input: Mnemonic, pc: u16) -> Result<Instruction, String> {
+    let mut output = Instruction {
+        opcode: 0x00,
+        operand: None,
+    };
 
-	dbg!(&input);
+    // BRK [impl]
+    if input.inst == "BRK" {
+    }
+    // ORA
+    else if input.inst == "ORA" {
+        // ORA (ind,X)
+        if input
+            .args
+            .clone()
+            .unwrap_or(String::from(""))
+            .starts_with("($")
+            && input
+                .args
+                .clone()
+                .unwrap_or(String::from(""))
+                .ends_with(",X)")
+        {
+            output.opcode = 0x01;
+            output.operand = Some(Operand::Half(
+                scan_fmt!(&input.args.clone().unwrap(), "(${},X)", [hex u8]).unwrap(),
+            ));
+        }
+        // ORA zpg
+        else if input
+            .args
+            .clone()
+            .unwrap_or(String::from(""))
+            .starts_with("$")
+            && input.args.clone().unwrap_or(String::from("")).len() <= 3
+        {
+            output.opcode = 0x05;
+            output.operand = Some(Operand::Half(
+                scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u8]).unwrap(),
+            ));
+        }
+        // ORA imm
+        else if input
+            .args
+            .clone()
+            .unwrap_or(String::from(""))
+            .starts_with("#")
+        {
+            if input
+                .args
+                .clone()
+                .unwrap_or(String::from(""))
+                .starts_with("#$")
+            {
+                output.opcode = 0x09;
+                output.operand = Some(Operand::Half(
+                    scan_fmt!(&input.args.clone().unwrap(), "#${}", [hex u8]).unwrap(),
+                ));
+            } else {
+                output.opcode = 0x09;
+                output.operand = Some(Operand::Half(
+                    scan_fmt!(&input.args.clone().unwrap(), "#{}", u8).unwrap(),
+                ));
+            }
+        }
+        // ORA abs
+        else if input
+            .args
+            .clone()
+            .unwrap_or(String::from(""))
+            .starts_with("$")
+            && input.args.clone().unwrap_or(String::from("")).len() >= 3
+        {
+            output.opcode = 0x0D;
+            output.operand = Some(Operand::Full(
+                scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u16]).unwrap(),
+            ));
+        } else {
+            return Err(String::from(
+                "The inputted mnemonic didn't match with anything",
+            ));
+        }
+    }
+    // ASL
+    else if input.inst == "ASL" {
+        // ASL zpg
+        if input
+            .args
+            .clone()
+            .unwrap_or(String::from(""))
+            .starts_with("$")
+            && input.args.clone().unwrap_or(String::from("")).len() <= 3
+        {
+            output.opcode = 0x06;
+            output.operand = Some(Operand::Half(
+                scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u8]).unwrap(),
+            ));
+        }
+        // ASL [accum]
+        else if input.args.clone().unwrap_or(String::from("")) == ""
+            || input.args.clone().unwrap_or(String::from("")) == "A"
+        {
+            output.opcode = 0x0A;
+            output.operand = None;
+        }
+        // ASL abs
+        else if input
+            .args
+            .clone()
+            .unwrap_or(String::from(""))
+            .starts_with("$")
+            && input.args.clone().unwrap_or(String::from("")).len() >= 3
+        {
+            output.opcode = 0x0E;
+            output.operand = Some(Operand::Full(
+                scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u16]).unwrap(),
+            ));
+        } else {
+            return Err(String::from(
+                "The inputted mnemonic didn't match with anything",
+            ));
+        }
+    }
+    // PHP [impl]
+    else if input.inst == "PHP" {
+        output.opcode = 0x08;
+        output.operand = None;
+    }
+    // BPL rel
+    else if input.inst == "BPL"
+        && input
+            .args
+            .clone()
+            .unwrap_or(String::from(""))
+            .starts_with("$")
+        && input.args.clone().unwrap_or(String::from("")).len() >= 3
+    {
+        output.opcode = 0x10;
+        // What is this
+        output.operand = Some(Operand::Half(
+            (scan_fmt!(&input.args.clone().unwrap(), "${}", [hex i16]).unwrap()
+                - i16::try_from(pc + 2).unwrap()) as u8,
+        ));
+    }
+    // *Incredibly* basic errors
+    else {
+        return Err(String::from(
+            "The inputted mnemonic didn't match with anything",
+        ));
+    }
 
-	// BRK [impl]
-	if input.inst == "BRK" {}
-
-	// ORA
-	else if input.inst == "ORA" {
-		// ORA (ind,X)
-		if input.args.clone().unwrap_or(String::from("")).starts_with("($") && input.args.clone().unwrap_or(String::from("")).ends_with(",X)") {
-			output.opcode = 0x01;
-			output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "(${},X)", [hex u8]).unwrap()));
-		}
-		// ORA zpg
-		else if input.args.clone().unwrap_or(String::from("")).starts_with("$") && input.args.clone().unwrap_or(String::from("")).len() <= 3 {
-			output.opcode = 0x05;
-			output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u8]).unwrap()));
-		}
-		// ORA imm
-		else if input.args.clone().unwrap_or(String::from("")).starts_with("#") {
-			if input.args.clone().unwrap_or(String::from("")).starts_with("#$") {
-				output.opcode = 0x09;
-				output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "#${}", [hex u8]).unwrap()));
-			}
-			else {
-				output.opcode = 0x09;
-				output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "#{}", u8).unwrap()));
-			}
-		}
-		// ORA abs
-		else if input.args.clone().unwrap_or(String::from("")).starts_with("$")  && input.args.clone().unwrap_or(String::from("")).len() >= 3 {
-			output.opcode = 0x0D;
-			output.operand = Some(Operand::Full(scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u16]).unwrap()));
-		}
-		else {
-			return Err(String::from("The inputted mnemonic didn't match with anything"));
-		}
-	}
-
-	// ASL
-	else if input.inst == "ASL" {
-		// ASL zpg
-		if input.args.clone().unwrap_or(String::from("")).starts_with("$") && input.args.clone().unwrap_or(String::from("")).len() <= 3 {
-			output.opcode = 0x06;
-			output.operand = Some(Operand::Half(scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u8]).unwrap()));
-		}
-		// ASL [accum]
-		else if input.args.clone().unwrap_or(String::from("")) == "" || input.args.clone().unwrap_or(String::from("")) == "A" {
-			output.opcode = 0x0A;
-			output.operand = None;
-		}
-		// ASL abs
-		else if input.args.clone().unwrap_or(String::from("")).starts_with("$")  && input.args.clone().unwrap_or(String::from("")).len() >= 3 {
-			output.opcode = 0x0E;
-			output.operand = Some(Operand::Full(scan_fmt!(&input.args.clone().unwrap(), "${}", [hex u16]).unwrap()));
-		} 
-		else {
-			return Err(String::from("The inputted mnemonic didn't match with anything"));
-		}
-	}
-
-	// PHP [impl]
-	else if input.inst == "PHP" {
-		output.opcode = 0x08;
-		output.operand = None;
-	}
-
-
-	// *Incredibly* basic errors
-	else {
-		return Err(String::from("The inputted mnemonic didn't match with anything"));
-	}
-	
-	Ok(output)
+    Ok(output)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,98 @@
 use rs_6502::*;
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+use std::path::Path;
+
+#[macro_use]
+extern crate scan_fmt;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    let input = Mnemonic {
-		inst: args[1].clone().to_uppercase(),
-		args: Some(args[2].clone().to_uppercase()),
-	};
+    let input_path: &String;
+    let output_path = Path::new("./a.out");
 
-	let output: Instruction;
+    if args.len() >= 2 {
+        input_path = &args[1];
+    } else {
+        println!("Not enough arguments given");
+        return;
+    }
 
-	match assemble(input) {
-		Ok(x) => output = x,
-		Err(e) => { println!("{}", e); return; },
-	}
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut output_file = match File::create(&output_path) {
+        Err(why) => panic!("couldn't create {}: {}", output_path.display(), why),
+        Ok(file) => file,
+    };
 
-	dbg!(output);
+    let mut output: Instruction;
+    let mut input: Mnemonic;
+    let mut pc: u16 = 0x0000;
+
+    if let Ok(lines) = read_lines(input_path) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(line) = line {
+                let (tempa, tempb) = scan_fmt_some!(&line, "{} {}", String, String);
+
+                match tempb {
+                    Some(x) => {
+                        input = Mnemonic {
+                            inst: tempa.unwrap(),
+                            args: Some(x),
+                        }
+                    }
+                    None => {
+                        input = Mnemonic {
+                            inst: tempa.unwrap(),
+                            args: None,
+                        }
+                    }
+                }
+
+                match assemble(input, pc) {
+                    Ok(x) => {
+                        output = x;
+                        pc = calc_new_pc(pc, &output);
+                    }
+                    Err(e) => {
+                        println!("{}", e);
+                        return;
+                    }
+                }
+
+                output_file
+                    .write(&output.opcode.to_le_bytes())
+                    .expect("write failed");
+
+                match &output.operand.unwrap() {
+                    Operand::Full(x) => {
+                        output_file.write(&x.to_le_bytes()).expect("write failed");
+                    }
+                    Operand::Half(x) => {
+                        output_file.write(&x.to_le_bytes()).expect("write failed");
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn calc_new_pc(pc: u16, input: &Instruction) -> u16 {
+    match &input.operand {
+        Some(x) => match x {
+            Operand::Half(..) => pc + 1 + 1,
+            Operand::Full(..) => pc + 1 + 2,
+        },
+        _ => pc + 1,
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
